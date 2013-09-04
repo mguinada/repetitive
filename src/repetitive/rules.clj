04a6ae51e7b620dec22764e37a48e0458547f6a7
Miguel Guinada <mguinada@gmail.com>
2013-09-04 21:46:11 +0100

    Implements monthly rule recurrence by day of week

--- src/repetitive/rules.clj
+++ src/repetitive/rules.clj
@@ -7,17 +7,35 @@
     (set (distinct values))
     (set (vector values))))
 
+(defn- check-day
+  [cal {:keys [days]}]
+  (contains? (to-set days) (c/day cal)))
+
+;;TODO: Validate the input AND also consider week days as numbers numbers
+(defn- check-day-of-week
+  [cal {days-of-week :days-of-week}]
+  (contains? (to-set days-of-week) (c/day-of-week cal)))
+
+(defn- compose-predicate-fn
+  "Creates a predicate function that returns true if any of the 
+   given functions eval to true against a given calendar date and a rule.
+   Otherwise nil is returned"
+  [rule & fns]
+  (fn [cal]
+    (some #(= (apply % [cal rule]) true) fns)))
+
 (defprotocol Rule
   (predicate [rule] "Rule predicate"))
 
-(defrecord MonthlyRule [days])
+(defrecord MonthlyRule [days days-of-week])
 (defrecord DailyRule [])
 
 (extend-type MonthlyRule
   Rule
-  (predicate [{days :days}]
-    (fn [cal]
-      (contains? (to-set days) (c/day cal)))))
+  (predicate [rule]
+    (compose-predicate-fn rule
+      check-day
+      check-day-of-week)))
 
 (extend-type DailyRule
   Rule
@@ -26,8 +44,10 @@
 
 (defn monthly-rule
   "Defines a monthly ocurrence rule"
-  [& {:keys [days] :or {days 1} :as args}]
-  (MonthlyRule. days))
+  [& {:keys [days days-of-week]}]
+  (if (and (nil? days) (nil? days-of-week))
+    (MonthlyRule. 1 days-of-week)
+    (MonthlyRule. days days-of-week)))
 
 (defn daily-rule
   "Defines a daily rule ocurrence"
--- test/repetitive/rule_test.clj
+++ test/repetitive/rule_test.clj
@@ -16,11 +16,22 @@
 (deftest monthly-rule-test
   (fact "recurs on a monthly basis"
     (take 3 (rule-occurrences (monthly-rule :days 15) :start-date t)) => [(set-calendar 2013 1 15)
-                                                                         (set-calendar 2013 2 15)
-                                                                         (set-calendar 2013 3 15)])
+                                                                          (set-calendar 2013 2 15)
+                                                                          (set-calendar 2013 3 15)])
+
+  (fact "recurs per day of week"
+    (take 4 (rule-occurrences (monthly-rule :days-of-week [:monday :friday]) :start-date t)) => [(set-calendar 2013 1 4)
+                                                                                                 (set-calendar 2013 1 7)
+                                                                                                 (set-calendar 2013 1 11)
+                                                                                                 (set-calendar 2013 1 14)])
 
   (fact "supports multiple days"
     (take 4 (rule-occurrences (monthly-rule :days [10 20]) :start-date t)) => [(set-calendar 2013 1 10)
-                                                                              (set-calendar 2013 1 20)
-                                                                              (set-calendar 2013 2 10)
-                                                                              (set-calendar 2013 2 20)]))
+                                                                               (set-calendar 2013 1 20)
+                                                                               (set-calendar 2013 2 10)
+                                                                               (set-calendar 2013 2 20)])
+
+  (fact "defaults to 1st day of month"
+    (take 3 (rule-occurrences (monthly-rule) :start-date t)) =>[(set-calendar 2013 1 1)
+                                                                (set-calendar 2013 2 1)
+                                                                (set-calendar 2013 3 1)]))
