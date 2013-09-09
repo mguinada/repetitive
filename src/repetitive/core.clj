0131a501cc34c83c98e2851d7658f5879204d290
Miguel Guinada <mguinada@gmail.com>
2013-09-09 02:59:17 +0100

    Fixes rule-occurrences default arg value declaration

--- src/repetitive/core.clj
+++ src/repetitive/core.clj
@@ -13,7 +13,7 @@
   "Determins a given rule ocurrences.
    Can take as an optional arguement a :start-date to consider as the start data.
    If a :start-date is not given, the current data will be considered"
-  [rule & {:keys [start-date] :or [start-date (c/calendar)]}]
+  [rule & {:keys [start-date] :or {start-date (c/calendar)}}]
   (filter #(apply (predicate rule) [%]) (c/daily-stream start-date)))
 
 
