b00eab7cbc5d54a311bf46c9397d993d7a9fe1a7
Miguel Guinada <mguinada@gmail.com>
2014-01-19 17:04:07 +0000

    Using reloaded project template

--- project.clj
+++ project.clj
@@ -1,11 +1,10 @@
 (defproject repetitive "0.1.0-SNAPSHOT"
   :description "Date recurrence library"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "TODO: Choose a license"
+            :url "http://choosealicense.com/"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
-  :profiles {:dev {:source-paths ["dev"]
-                   :dependencies [[midje "1.5.1"]
-                                  [org.clojure/tools.namespace "0.2.4"]]
-                   :plugins [[lein-midje "3.1.0"]]}}
-  :repl-options {:init-ns repl-tools})
+  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.4"]
+                                  [midje "1.5.1"]]
+                   :source-paths ["dev"]
+                   :plugins [[lein-midje "3.1.0"]]}})
